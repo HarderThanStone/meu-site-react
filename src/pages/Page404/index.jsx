c927e88329867a22eefe0ee655dedbdd0c898519
Gabriel da Silva Crispim <gabrieldasilvacrispim2007@gmail.com>
2025-07-20 21:25:34 -0300

    novo commit final

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Container from '../../components/Container';
 
+
 function Page404() {
   return (
     <>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,28 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Projetos from './pages/Projetos';
 import Contatos from './pages/Contatos';
 import Page404 from './pages/Page404';
 
-<Route path="*" element={<Page404 />} />
-
-
-
 function AppRoutes() {
   return (
-    <BrowserRouter basename="/meu-site-react">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/projetos" element={<Projetos />} />
         <Route path="/contatos" element={<Contatos />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
 export default AppRoutes;
-
-
-    
